eeac06443c09a443a4e941310b7900676c176f48
Eesh Majithia <majithiae25@bolles.org>
2022-10-01 10:11:11 -0400

    fixed encoders coefficients back

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/botconfigs/PursuitBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/botconfigs/PursuitBot.java
@@ -51,9 +51,9 @@ public class PursuitBot {
         drive = new MecDriveFlip(motorFL, motorFR, motorBL, motorBR);
 
         // initialize odometry
-        encoderL = getSupplier(motorFL, 1);
-        encoderR = getSupplier(motorFR, -1);
-        encoderH = getSupplier(motorBL, 1);
+        encoderL = getSupplier(motorFL, -1);
+        encoderR = getSupplier(motorFR, 1);
+        encoderH = getSupplier(motorBL, -1);
         odometry = new OdometrySubsystem(new HolonomicOdometry(
                 encoderL, encoderR, encoderH,
                 encoderTrackWidth, encoderWheelOffset));
