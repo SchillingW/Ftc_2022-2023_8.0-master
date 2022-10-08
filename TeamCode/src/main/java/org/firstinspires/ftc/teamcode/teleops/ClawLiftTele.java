8510703034f2a14d8651465e2a9f628364cc05c4
Sarah_Park <parks25@bolles.org>
2022-10-08 10:36:17 -0400

    AUTO WORKS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/ClawLiftTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/ClawLiftTele.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.teleops;
 
+import static java.lang.Thread.sleep;
+
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.botconfigs.PursuitBot;
 import org.firstinspires.ftc.teamcode.hardware.GamepadSystem;
@@ -20,12 +23,14 @@ public class ClawLiftTele extends OpMode {
 
     public double turnSpeed = 0.75;
     public double linearSpeed = 0.75;
+    public ElapsedTime timer = new ElapsedTime();
 
     // motor declaration
-    public ServoEx clawL;
-    public ServoEx clawR;
-    public Motor armB;
-    public Motor armF;
+    //public ServoEx clawL;
+    //public ServoEx clawR;
+    //public Motor armB;
+    //public Motor armF;
+    public Motor slide;
 
     // input system reference
     GamepadSystem input;
@@ -37,13 +42,14 @@ public class ClawLiftTele extends OpMode {
         robot = new PursuitBot(telemetry, hardwareMap);
 
         // initialize hardware devices
-        clawL = new SimpleServo(hardwareMap, "clawL", 0, 180);
-        clawR = new SimpleServo(hardwareMap, "clawR", 0, 180);
-        armF = new Motor(hardwareMap, "armF");
-        armB = new Motor(hardwareMap, "armB");
+        //clawL = new SimpleServo(hardwareMap, "clawL", 0, 180);
+        //clawR = new SimpleServo(hardwareMap, "clawR", 0, 180);
 
-        armF.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-        armB.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        slide = new Motor(hardwareMap, "slide");
+
+        //armF.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        //armB.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        //slide.setZeroPowerBehavior(null);
 
         input = new GamepadSystem(this);
     }
@@ -57,23 +63,55 @@ public class ClawLiftTele extends OpMode {
                 input.gamepad1.getLeftX() * linearSpeed,
                 input.gamepad1.getRightX() * turnSpeed);
 
-        armF.set(input.gamepad2.getRightY() * armSpeed);
-        armB.set(input.gamepad2.getLeftY() * armSpeed);
+        if(gamepad2.y)
+        {
+            timer.reset();
 
+            Up();
+            Down();
+
+            timer.reset();
+            telemetry.addData("seconds", timer.seconds());
+        }
+        //armF.set(input.gamepad2.getRightY() * armSpeed);
+        //armB.set(input.gamepad2.getLeftY() * armSpeed);
+        slide.set(input.gamepad2.getRightY() * armSpeed);
+        telemetry.addData("speed", input.gamepad2.getRightY() * armSpeed);
+        //slide.set(input.gamepad2.getButton)
+        //telemetry.addData("Linear Slide Position", slide.getDistance());
         //full close=1
+
         if (input.gamepad2.getButton(GamepadKeys.Button.RIGHT_BUMPER)) {
-            clawL.setPosition(1);
-            clawR.setPosition(0);
+            //clawL.setPosition(1);
+            //clawR.setPosition(0);
             telemetry.addData("servo", "close");
             telemetry.update();
         }
         //full open=0
         if (input.gamepad2.getButton(GamepadKeys.Button.LEFT_BUMPER)) {
-            clawL.setPosition(0);
-            clawR.setPosition(1);
+            //clawL.setPosition(0);
+            //clawR.setPosition(1);
             telemetry.addData("servo", "open");
             telemetry.update();
         }
 
+    }
+
+    public void Up()
+    {
+        timer.reset();
+        while(timer.seconds() <= 10)
+        {
+            slide.set(-0.75);
+        }
+    }
+
+    public void Down()
+    {
+        timer.reset();
+        while(timer.seconds() <= 10)
+        {
+            slide.set(-0.75);
+        }
     }
 }
