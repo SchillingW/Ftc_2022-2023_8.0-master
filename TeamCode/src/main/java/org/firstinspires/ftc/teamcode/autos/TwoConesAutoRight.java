6e36eb8bf2c3e5dbd658f386652e342e0752bba2
Sarah_Park <parks25@bolles.org>
2022-12-20 11:02:56 -0500

    Linear slide while driving in auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/TwoConesAutoRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/TwoConesAutoRight.java
@@ -65,12 +65,12 @@ public class TwoConesAutoRight extends LinearOpMode {
         linearSlide.goToFull(linearSlide.high, telemetry, this);
         robot.reachPoint(new Pose2d(robot.xDim.toPole(2), robot.yDim.toPole(1), new Rotation2d()), telemetry, this);*/
 
-        robot.reachPoint(new Pose2d(robot.xDim.toCell(0), robot.yDim.toCell(4), new Rotation2d()), telemetry, this);
-        linearSlide.goToFull(linearSlide.med, telemetry, this);
-        robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(4), new Rotation2d()), telemetry, this);
-        robot.reachPoint(new Pose2d(robot.xDim.toPole(1), robot.yDim.toPole(3), new Rotation2d()), telemetry, this);
+        robot.reachPoint(new Pose2d(robot.xDim.toCell(0), robot.yDim.toCell(4), new Rotation2d()), telemetry, this, "medium");
+        //linearSlide.goToFull(linearSlide.med, telemetry, this);
+        robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(4), new Rotation2d()), telemetry, this, "medium");
+        robot.reachPoint(new Pose2d(robot.xDim.toPole(1), robot.yDim.toPole(3), new Rotation2d()), telemetry, this, "medium");
 
-        linearSlide.goToFull(linearSlide.med + dropOffset, telemetry, this);
+        //linearSlide.goToFull(linearSlide.med + dropOffset, telemetry, this);
         sleep(200);
         //if (opModeIsActive()) sleep(1000);
         if (opModeIsActive()) linearSlide.openClaw();
@@ -81,18 +81,18 @@ public class TwoConesAutoRight extends LinearOpMode {
         robot.reachPoint(new Pose2d(robot.xDim.toCell(2), robot.yDim.toCell(1), new Rotation2d()), telemetry, this);*/
         //robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(0), new Rotation2d()), telemetry, this);
         //robot.reachPoint(new Pose2d(robot.xDim.toCell(2), robot.yDim.toCell(2), new Rotation2d()), telemetry, this);
-        robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(4), new Rotation2d()), telemetry, this);
-        robot.reachPoint(new Pose2d(robot.xDim.toCell(2), robot.yDim.toCell(4), new Rotation2d()), telemetry, this);
-        robot.reachPoint(new Pose2d(robot.xDim.toCell(2), robot.yDim.toCell(5) - 4.5, new Rotation2d()), telemetry, this);
+        robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(4), new Rotation2d()), telemetry, this, null);
+        robot.reachPoint(new Pose2d(robot.xDim.toCell(2), robot.yDim.toCell(4), new Rotation2d()), telemetry, this, null);
+        robot.reachPoint(new Pose2d(robot.xDim.toCell(2), robot.yDim.toCell(5) - 4.5, new Rotation2d()), telemetry, this, null);
         if (opModeIsActive()) sleep(200);
         GrabConeFromCell(0);
-        robot.reachPoint(new Pose2d(robot.xDim.toCell(2), robot.yDim.toCell(4), new Rotation2d()), telemetry, this);
-        robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(4), new Rotation2d()), telemetry, this);
+        robot.reachPoint(new Pose2d(robot.xDim.toCell(2), robot.yDim.toCell(4), new Rotation2d()), telemetry, this, null);
+        robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(4), new Rotation2d()), telemetry, this, null);
 
         if(result == 1 || result == 2)
         {
             linearSlide.goToFull(linearSlide.med, telemetry, this);
-            robot.reachPoint(new Pose2d(robot.xDim.toPole(1), robot.yDim.toPole(3), new Rotation2d()), telemetry, this);
+            robot.reachPoint(new Pose2d(robot.xDim.toPole(1), robot.yDim.toPole(3), new Rotation2d()), telemetry, this, null);
             sleep(200);
             //if (opModeIsActive()) sleep(1000);
             if (opModeIsActive()) linearSlide.openClaw();
@@ -102,7 +102,7 @@ public class TwoConesAutoRight extends LinearOpMode {
         else
         {
             linearSlide.goToFull(linearSlide.low, telemetry, this);
-            robot.reachPoint(new Pose2d(robot.xDim.toPole(1), robot.yDim.toPole(4), new Rotation2d()), telemetry, this);
+            robot.reachPoint(new Pose2d(robot.xDim.toPole(1), robot.yDim.toPole(4), new Rotation2d()), telemetry, this, null);
             sleep(200);
             //if (opModeIsActive()) sleep(1000);
             if (opModeIsActive()) linearSlide.openClaw();
@@ -110,7 +110,7 @@ public class TwoConesAutoRight extends LinearOpMode {
         }
 
         // PARK
-        robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(result + 3), new Rotation2d()), telemetry, this);
+        robot.reachPoint(new Pose2d(robot.xDim.toCell(1), robot.yDim.toCell(result + 3), new Rotation2d()), telemetry, this, null);
         linearSlide.goToFull(linearSlide.ground, telemetry, this);
     }
 
@@ -124,7 +124,7 @@ public class TwoConesAutoRight extends LinearOpMode {
         sleep(500);
         linearSlide.goToFull(linearSlide.low, telemetry, this);
         robot.TranslateY(-0.55, -0.2, telemetry, this);
-        robot.reachPoint(new Pose2d(robot.odometry.getPose().getX(), robot.odometry.getPose().getY(), new Rotation2d()), telemetry, this);
+        robot.reachPoint(new Pose2d(robot.odometry.getPose().getX(), robot.odometry.getPose().getY(), new Rotation2d()), telemetry, this, null);
     }
 
     public void Cycle(int i)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/botconfigs/PursuitBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/botconfigs/PursuitBot.java
@@ -13,11 +13,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.botconfigs.LinearSlide;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.FieldDimensions;
 import org.firstinspires.ftc.teamcode.hardware.MecDriveFlip;
 
+
+import java.util.Optional;
 import java.util.function.DoubleSupplier;
 
 // mecanum drive bot with odometry for Pure Pursuit
@@ -31,6 +34,7 @@ public class PursuitBot {
     public Telemetry tele;
 
     public ColorSensor sensor;
+    public int dropOffset = 80;
 
     // mecanum wheel drive train
     public MecanumDrive drive;
@@ -38,6 +42,7 @@ public class PursuitBot {
     public Motor motorFR;
     public Motor motorBL;
     public Motor motorBR;
+    public LinearSlide linearSlide;
 
     // odometry device
     public OdometrySubsystem odometry;
@@ -82,6 +87,7 @@ public class PursuitBot {
         motorFR = new Motor(map, "motorFR");
         motorBL = new Motor(map, "motorBL");
         motorBR = new Motor(map, "motorBR");
+        linearSlide = new LinearSlide(tele, map);
 
         //sensor = map.colorSensor.get("sensor");
 
@@ -116,14 +122,23 @@ public class PursuitBot {
 
 
 
-    public void reachPoint(Pose2d target, Telemetry tele, LinearOpMode mode) {
+    public void reachPoint(Pose2d target, Telemetry tele, LinearOpMode mode, String level) {
+        Optional<String> lname = Optional.ofNullable(level);
 
         if (mode.opModeIsActive()) {
 
             odometry.update();
 
             while (!isAtTarget(target) && mode.opModeIsActive()) {
-
+                if (level == "low") {
+                    linearSlide.goTo(linearSlide.low + dropOffset, telemetry);
+                }
+                if (level == "medium") {
+                    linearSlide.goTo(linearSlide.med, telemetry);
+                }
+                if (level == "high") {
+                    linearSlide.goTo(linearSlide.high, telemetry);
+                }
                 odometry.update();
                 moveTowards(true, target, tele);
             }
