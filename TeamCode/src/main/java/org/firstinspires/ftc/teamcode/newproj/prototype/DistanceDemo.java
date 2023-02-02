431676861b8cac87ff80fd65e15d4f0fcd7b0aeb
LiamLime_SR <wschilling003@gmail.com>
2023-02-02 17:09:36 -0500

    distance demo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newproj/prototype/DistanceDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newproj/prototype/DistanceDemo.java
@@ -5,21 +5,52 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.newproj.util.VectorRotate;
 
 @TeleOp(name="DistanceDemo",group="LeaguePrep")
 public class DistanceDemo extends OpMode {
 
-    public DistanceSensor distance;
+    public DistanceSensor distance1;
+    public DistanceSensor distance2;
 
     @Override
     public void init() {
 
-        distance = hardwareMap.get(DistanceSensor.class, "sensor");
+        distance1 = hardwareMap.get(DistanceSensor.class, "sensor1");
+        distance2 = hardwareMap.get(DistanceSensor.class, "sensor2");
     }
 
     @Override
     public void loop() {
 
-        telemetry.addData("distance", String.format("%.2f", distance.getDistance(DistanceUnit.INCH)));
+        double dist1 = distance1.getDistance(DistanceUnit.INCH);
+        double dist2 = distance2.getDistance(DistanceUnit.INCH);
+        double[] pos = getPos(dist1, dist2, 1.3, 1.6);
+        telemetry.addData("X", String.format("%.2f", pos[0]));
+        telemetry.addData("Y", String.format("%.2f", pos[1]));
+    }
+
+    public double[] getPos(double dist1, double dist2, double offset, double radius) {
+
+        double x1 = -offset / 2;
+        double y1 = dist1;
+        double x2 = offset / 2;
+        double y2 = dist2;
+
+        double dispX = x2 - x1;
+        double dispY = y2 - y1;
+        double theta = Math.atan(dispY / dispX) + (dispX < 0 ? Math.PI : 0);
+        double d = Math.sqrt(dispX * dispX + dispY * dispY);
+
+        double localX = d / 2;
+        double localY = Math.sqrt(radius * radius - localX * localX);
+
+        double middleX = VectorRotate.rotX(localX, localY, theta);
+        double middleY = VectorRotate.rotY(localX, localY, theta);
+
+        double finalX = middleX + x1;
+        double finalY = middleY + y1;
+
+        return new double[] {finalX, finalY};
     }
 }
